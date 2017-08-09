e32c8ea8bfbf7b74948f43592ed9056d7a909427
Jean-Philippe Ruijs <jean-philippe@ruijs.fr>
2017-08-09 18:08:56 +0200

    hidden from admin menu, hijacks "add new" button from page overview

--- NewPage.php
+++ NewPage.php
@@ -8,21 +8,38 @@
  * Author URI: https://github.com/jeanphilipperuijs/
  * License: GPL2
  */
-namespace MPAT\NewPageWizard;
-
-class NewPage
-{
-    function init()
-    {
-        add_menu_page('Wizard', 'Wizard', 'manage_options', 'MPAT_NewPageWizard', array(&$this, 'js'), 'dashicons-welcome-learn-more');
-    }
-    function js()
-    {
-        wp_enqueue_script('wp-api');
-   //     wp_enqueue_script('mpat-newpage-wizard', 'http://localhost:8000/rui.js', array('wp-api'), 1.0, true );
-        wp_enqueue_script('mpat-newpage-wizard', plugin_dir_url(__FILE__) . 'public/rui.js', array('wp-api'), 1.0, true );
-    }
-}
-
-$np = new NewPage();
-add_action("admin_menu", array(&$np, "init"));
+ namespace MPAT\NewPageWizard;
+ 
+ class NewPage
+ {
+     function init()
+     {
+ //      add_menu_page('Wizard', 'Wizard', 'manage_options', 'MPAT_NewPageWizard', array(&$this, 'js'), 'dashicons-welcome-learn-more');
+         add_submenu_page('_doesntexist','Wizard','','manage_options', 'MPAT_NewPageWizard', array(&$this, 'js'), 'dashicons-welcome-learn-more');
+         
+         if (isset($_GET['post_type']) && $_GET['post_type'] === 'page' ) {
+             ?>
+         <script>
+             window.onload = function() {
+                 let post_type = '<?php print $_GET['post_type']; ?>';
+                 try{
+                     let l = document.getElementsByClassName('page-title-action')[0];
+                     l.href='./admin.php?page=MPAT_NewPageWizard';
+                 }catch(err){
+                     console.log(err);
+                 }
+             }
+         </script>
+         <?php
+         }
+     }
+     function js()
+     {
+         wp_enqueue_script('wp-api');
+         wp_enqueue_script('mpat-newpage-wizard', plugin_dir_url(__FILE__) . 'public/rui.js', array('wp-api'), 1.0, true );
+     }
+ }
+ 
+ $np = new NewPage();
+ add_action("admin_menu",array(&$np, "init") );
+ 
