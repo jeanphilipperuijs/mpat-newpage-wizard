6e9feace229f35b3e9b33c6bcc5d161527e3516b
Jean-Philippe Ruijs <jean-philippe@ruijs.fr>
2017-08-24 14:06:51 +0200

    supplementary button for wizard on page overview

--- NewPage.php
+++ NewPage.php
@@ -7,22 +7,33 @@
  * Author: Jean-Philippe Ruijs
  * Author URI: https://github.com/jeanphilipperuijs/
  * License: GPL2
+ * Text Domain:    	mpat-newpage-wizard
  */
- namespace MPAT\NewPageWizard;
+namespace MPAT\NewPageWizard;
  
 class NewPage
 {
     function init()
     {
-        add_submenu_page('_doesntexist', 'Wizard', '', 'manage_options', 'MPAT_NewPageWizard', array(&$this, 'js'), 'dashicons-welcome-learn-more');
+
+		load_plugin_textdomain('mpat-newpage-wizard', false, basename( dirname( __FILE__ ) ) . '/languages' );
+        add_submenu_page('_doesntexist', __("Wizard", "mpat-newpage-wizard"), '', 'manage_options', 'MPAT_NewPageWizard', array(&$this, 'js'), 'dashicons-welcome-learn-more');
          
         if (isset($_GET['post_type']) && $_GET['post_type'] === 'page') {
             ?>
          <script>
              window.onload = function() {
                 try{
-                    let l = document.getElementsByClassName('page-title-action')[0];
-                    l.href='./admin.php?page=MPAT_NewPageWizard';
+                    let addNew = document.getElementsByClassName('page-title-action')[0];
+                    //addNew.innerText = '<?php _e("New Page Editor", "mpat-newpage-wizard"); ?>';
+                    //addNew.href='./admin.php?page=MPAT_NewPageWizard';
+                    
+                    let n = document.getElementsByClassName('wp-heading-inline')[0];
+                    let wizard = document.createElement('a');
+                    wizard.href='./admin.php?page=MPAT_NewPageWizard';
+                    wizard.innerText = '<?php _e("Wizard", "mpat-newpage-wizard"); ?>';
+                    wizard.className = 'page-title-action';
+                    n.appendChild(wizard);
                 }catch(err){
                     console.log('NewPageWizard', err);
                 }
