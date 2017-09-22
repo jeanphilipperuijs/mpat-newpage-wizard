46b26aa0f0f86a3e34d9338e126193bb4964bac0
Jean-Philippe Ruijs <jean-philippe.ruijs@telecom-paristech.fr>
2017-09-22 13:10:01 +0200

    uglify

--- webpack.config.js
+++ webpack.config.js
@@ -10,8 +10,9 @@ module.exports = {
   plugins: [
     new webpack.DefinePlugin(
       { 'process.env': { 'NODE_ENV': JSON.stringify('production') } }),
-    /*  new webpack.optimize.UglifyJsPlugin({compress: {warnings: true}
-    })*/
+    new webpack.optimize.UglifyJsPlugin({
+      compress: { warnings: true }
+    })
   ],
   module: {
     loaders: [{
